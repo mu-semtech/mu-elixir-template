db27c473a67f90174aee81e44fb86039e0ce1553
Jonathan Langens <flowofcontrol@gmail.com>
2019-08-25 15:28:43 +0200

    Added extra applications (http poison and poison) and updated .gitignore

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Elixirtemplate.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :httpoison, :poison],
       mod: {Elixirtemplate.Application, []}
     ]
   end
